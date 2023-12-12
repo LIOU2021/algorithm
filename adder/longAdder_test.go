2d289d32cca60382579e8b55f52c8f82d5cd27b1
LIOU2021 <liou95145hf@gmail.com>
2023-12-12 10:17:26 +0800

    fix

--- adder/longAdder_test.go
+++ adder/longAdder_test.go
@@ -3,22 +3,26 @@ package adder
 
 import (
 	"fmt"
-	"time"
+	"sync"
+	"testing"
 
 	ga "go.linecorp.com/garr/adder"
 )
 
-func Test_LongAdder() {
+func Test_LongAdder(t *testing.T) {
 	// or ga.DefaultAdder() which uses jdk long-adder as default
 	adder := ga.NewLongAdder(ga.JDKAdderType)
 
+	wg := sync.WaitGroup{}
+	wg.Add(100)
 	for i := 0; i < 100; i++ {
 		go func() {
 			adder.Add(123)
+			wg.Done()
 		}()
 	}
 
-	time.Sleep(1 * time.Second)
+	wg.Wait()
 
 	// get total added value
 	fmt.Println(adder.Sum())
