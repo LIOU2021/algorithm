1f55ebfac9ae0a7011586fef239c4af06f0df65e
LIOU2021 <liou95145hf@gmail.com>
2023-12-12 10:24:49 +0800

    feat long adder test

--- adder/longAdder_test.go
+++ adder/longAdder_test.go
@@ -4,7 +4,9 @@ package adder
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	ga "go.linecorp.com/garr/adder"
 )
@@ -14,8 +16,13 @@ func Test_LongAdder(t *testing.T) {
 	adder := ga.NewLongAdder(ga.JDKAdderType)
 
 	wg := sync.WaitGroup{}
-	wg.Add(100)
-	for i := 0; i < 100; i++ {
+
+	n := 1000000
+	wg.Add(n)
+
+	now := time.Now()
+
+	for i := 0; i < n; i++ {
 		go func() {
 			adder.Add(123)
 			wg.Done()
@@ -25,5 +32,28 @@ func Test_LongAdder(t *testing.T) {
 	wg.Wait()
 
 	// get total added value
-	fmt.Println(adder.Sum())
+	fmt.Printf("Test_LongAdder - sum: %d, du: %s\n", adder.Sum(), time.Since(now))
+}
+
+func Test_NormalAdder(t *testing.T) {
+
+	wg := sync.WaitGroup{}
+	var sum int64
+
+	n := 1000000
+	wg.Add(n)
+
+	now := time.Now()
+
+	for i := 0; i < n; i++ {
+		go func() {
+			atomic.AddInt64(&sum, 123)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	// get total added value
+	fmt.Printf("Test_NormalAdder - sum: %d, du: %s\n", sum, time.Since(now))
 }
