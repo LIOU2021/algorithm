fcbf404540b8c2612239a8208bbf7ba43ebc0506
LIOU2021 <aa78789898tw@gmail.com>
2024-06-12 10:36:01 +0800

    doc

--- hash/jump.go
+++ hash/jump.go
@@ -6,6 +6,7 @@ import (
 
 // 一致性hash
 // jump hash
+// ref: https://blog.csdn.net/ltochange/article/details/121294393
 func Jump(key uint64, numBuckets int) int32 {
 	return jump.Hash(key, numBuckets)
 }
